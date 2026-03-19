78e8bb04fdd895b0b9e08d21aa93ab38048f5dfa
neya-mul <neyamulislam946@gmail.com>
2026-03-19 21:57:46 +0600

    country area added and big and small country showed

--- src/components/countries.jsx
+++ src/components/countries.jsx
@@ -2,17 +2,19 @@ import { use } from "react";
 import Country from "./country";
 
 export default function Countries({ allCountries }) {
-    const useCountriesData = use(allCountries)
-    const useCountries = useCountriesData.countries
-    // console.log(useCountries);
+  const useCountriesData = use(allCountries)
+  const useCountries = useCountriesData.countries
+  // console.log(useCountries);
 
-    return (
-        <div>
-            {/* <h1>Neyamul</h1> */}
-          {
-            useCountries.map(country=> <Country  country={country}></Country>)
-          }
-        </div>
-    );
+  return (
+    <div className="text-center space-y-4">
+      <h1 className="text-5xl font-bold">All countries:</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3">
+        {
+          useCountries.map(country => <Country country={country} ></Country>)
+        }
+      </div>
+    </div>
+  );
 }
 
--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,13 +1,15 @@
 
 export default function Country({country}) {
-    console.log(country.flags.flags);
+    console.log(country.area.area);
 
     
   return (
     
-    <div className="border border-red-400 max-w-[500px] mb-2 p-4 mx-auto">
+    <div className="border border-red-400 max-w-[500px] mb-2 p-4 mx-auto text-center">
         <img src={country.flags.flags.png} className="mx-auto" alt="" />
-        <h1 className="text-center">Name: {country.name.common}</h1>
+        <h1 className="">Name: {country.name.common}</h1>
+        <p>Area: {country.area.area}</p>
+        <p>{country.area.area >= 400000 ? 'Big country' : 'Small Country'}</p>
 
     </div>
   );
